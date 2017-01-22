35f1c0636916a656aa1d7653526ff24877e9b369
Bill Gassoway <fbcwg123@gmail.com>
2017-01-22 23:50:05 +0000

    Fix migrations

--- db/migrate/20170121000751_fix_friendships.rb
+++
@@ -1,8 +0,0 @@
-class FixFriendships < ActiveRecord::Migration[5.0]
-  def change
-    change_table :friendships do |t|
-      t.belongs_to :user
-      t.belongs_to :friend, class: 'User'
-    end
-  end
-end
